007f9f9ab6022e5cca21b13e2507d87728680197
Piotr <43279721+xpersky@users.noreply.github.com>
2020-10-22 13:11:07 +0200

    fixes

--- Weather/Block/Page/Footer/Weather.php
+++ Weather/Block/Page/Footer/Weather.php
@@ -8,15 +8,17 @@ use Magento\Framework\View\Element\Template\Context;
 class Weather extends Template
 {
     protected $_entryFactory;
+    protected $WeatherData;
 
     public function __construct(
         Context $context,
         array $data = []) 
     {
         parent::__construct($context, $data);
+        $this->getData();
     }
-
-    public function getHtml()
+    
+    public function getData()
     {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
@@ -24,21 +26,26 @@ class Weather extends Template
         $tablename = $resource->getTableName('weather_updates');
         $query = "SELECT * from $tablename order by timestamp DESC limit 1";
         $data = $connection->fetchAll($query);
-        $html = '';
-        $temp = $data[0]['temperature'];
-        $html .= "<span style=\"margin-right:1rem;\">$temp &#x2103;</span>";
-        $icon = $data[0]['icon'];
-        $url = "http://openweathermap.org/img/w/" . $icon . ".png";
-        $html .= "<span style=\"margin-right:1rem;\"><img style=\"width:20px;\" src=\"$url\" alt=\"icon\"></span>";
-        $desc = $data[0]['description'];
-        $html .= "<span style=\"margin-right:1rem;\">$desc</span>";
-        $pressure = $data[0]['pressure'];
-        $html .= "<span style=\"margin-right:1rem;\">$pressure hPa</span>";
-        return $html;
+        $this->WeatherData = $data[0];
     }
 
-    public function getWeather()
+    public function getTemperature()
     {
-        return __($this->getHtml());
+        return __($this->WeatherData['temperature']);
+    }
+    
+    public function getIcon()
+    {
+        return __('http://openweathermap.org/img/w/'.$this->WeatherData['icon'].'.png');
+    }
+    
+    public function getDescription()
+    {
+        return __($this->WeatherData['description']);
+    }
+    
+    public function getPressure()
+    {
+        return __($this->WeatherData['pressure']);
     }
 }
